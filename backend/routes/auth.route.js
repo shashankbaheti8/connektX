e8216e7817189f90713d4148e3194c83e98630dd
shashankbaheti8 <shashankbaheti8@gmail.com>
2025-06-27 17:01:10 +0530

    auth-backend

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,10 +1,20 @@
 import { Router } from "express";
-import { Login, Logout, Signup } from "../controllers/auth.controller.js";
+import {
+  Login,
+  Logout,
+  Signup,
+  Onboard,
+} from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = Router();
 
 router.post("/signup", Signup);
 router.post("/login", Login);
 router.post("/logout", Logout);
+router.post("/onboard", protectRoute, Onboard);
 
+router.get("/me", protectRoute, (req, res) => {
+  res.status(200).json({ success: true, user: req.user });
+});
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js";
 import { connectDB } from "./lib/db.js";
 dotenv.config();
+
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 
